dba1aab44ab7e10853d38d4a8f9e8f6de000b6fc
Jeremy Hindle <mentioummetis@gmail.com>
2012-07-20 18:04:58 +0100

    Shader compile and program.

--- WebGl.js
+++ WebGl.js
@@ -12,6 +12,39 @@ function WbGl(CID, FSID, VSID){
         this.AspectRatio = canvas.width / canvas.height;
 
         //Load Shaders Here
-    }
+        //
+        var FShader = document.getElementById(FSID); //vertex shader
+        var VShader = document.getElementById(VSID); //fragment shader
 
+        if(!FShader || !VShader)
+            alert("Error, Could Not Find Shaders");
+        else{
+            //Load and compule fragment shader
+            var Code = LoadShader(FShader);
+            FShader = this.GL.createShader(this.GL.FRAGMENT_SHADER);
+            this.GL.shaderSource(FShader, Code);
+            this.GL.compileShader(FShader);
+
+            //Load and compule vertex shader
+            Code = LoadShader(VShader);
+            VShader = this.GL.createShader(this.GL.FRAGMENT_SHADER);
+            this.GL.shaderSource(VShader, Code);
+            this.GL.compileShader(VShader);
+
+            //Create the shader program
+            this.ShaderProgram = this.GL.createProgram();
+            this.GL.attachShader(this.ShaderProgram, FShader);
+            this.GL.attachShader(this.ShaderProgram, VShader);
+            this.GL.linkProgram(this.ShaderProgram);
+            this.GL.useProgram(this.ShaderProgram);
+
+            //Link vertex position attribute from shader
+            this.VertexPosition = this.getAttribLocation(this.ShaderProgram, "VertexPosition");
+            this.VertexPosition = this.GL.getAttribLocation(this.VertexPosition);
+
+            //Link texture coordinate attribute from shader
+            this.VertexTexture = this.GL.getAttribLocation(this.ShaderProgram, "TextureCoord");
+            this.GL.enableVertexAttribArray(this.VertexTexture);
+        }
+    }
 }
